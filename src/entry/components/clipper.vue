d31f91f7bc199d8cabae007e903eb2551f0092dc
李昕亮 <lixinliang@yy.com>
2016-11-17 09:14:59 +0800

    add
    clipper

---
+++ src/entry/components/clipper.vue
@@ -0,0 +1,106 @@
+<script>
+import clipper from  '../modules/clipper.js';
+
+const appIconSize = 180;
+const canvas = document.createElement('canvas');
+const context = canvas.getContext('2d');
+canvas.width = canvas.height = appIconSize;
+clipper.init({
+	canvas,
+	context,
+});
+
+let scale;
+let rotation;
+
+export default {
+    props : ['clipping'],
+	ready () {
+        this.$els.canvas.appendChild(canvas);
+	},
+	methods : {
+        panend ( event ) {
+            clipper.save('translate');
+            clipper.render();
+        },
+        panmove ( event ) {
+            clipper.deltaX = event.deltaX;
+            clipper.deltaY = event.deltaY;
+            clipper.render();
+        },
+        pinchend ( event ) {
+            clipper.save('scale');
+            clipper.save('rotation');
+            clipper.save('translate');
+            clipper.render();
+        },
+        pinchmove ( event ) {
+            clipper.scale = event.scale / scale;
+            clipper.rotation = event.rotation - rotation;
+            clipper.render();
+        },
+        pinchstart ( event ) {
+            scale = event.scale;
+            rotation = event.rotation;
+        },
+	},
+};
+</script>
+
+<template>
+	<div class="clipper">
+        <div class="clipper-icon"
+            v-el:canvas
+            v-show="clipping"
+            transition="fade"
+            v-touch:panend="panend"
+            v-touch:panmove="panmove"
+            v-touch:pinchend="pinchend"
+            v-touch:pinchmove="pinchmove"
+            v-touch:pinchstart="pinchstart"
+        ></div>
+	</div>
+</template>
+
+<style lang="scss">
+    .clipper {
+        position: relative;
+        z-index: 1;
+        top: 50px + 15 + 50 + 10;
+        @media only screen and (max-width: 414px) {
+            & {
+                top: 36px + 15 + 50 + 10;
+            }
+        }
+    }
+    .clipper-icon {
+        position: absolute;
+        width: 80%;
+        padding-top: 80%;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        overflow: hidden;
+        border-radius: 15%;
+        canvas,
+        &::after {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            top: 0;
+            left: 0;
+        }
+        &::after {
+            content: "";
+            z-index: 3;
+            pointer-events: none;
+            background-size: 100% 100%;
+            background-repeat: no-repeat;
+            background-image: url(../img/icon.png);
+            overflow: hidden;
+            border-radius: 15%;
+            border: 1px solid #9f9f9f;
+            opacity: .7;
+        }
+    }
+</style>
