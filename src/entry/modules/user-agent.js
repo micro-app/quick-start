38ae7a8f1a50701fca6a6cd903b7a9c36d9ae539
李昕亮 <lixinliang@yy.com>
2016-11-19 00:30:18 +0800

    update
    user agent

--- src/entry/modules/user-agent.js
+++ src/entry/modules/user-agent.js
@@ -1,6 +1,19 @@
-let userAgent = navigator.userAgent;
+const userAgent = navigator.userAgent;
 
-exports.ios = /\(i[^;]+;( U;)? CPU.+Mac OS X/i.test(userAgent);
-exports.mobile = /Android|webOS|iPhone|Windows Phone|iPod|iPad|BlackBerry|SymbianOS/i.test(userAgent);
-exports.safari = /\bversion\/([0-9.]+(?: beta)?)(?: mobile(?:\/[a-z0-9]+)?)? safari\//i.test(userAgent);
-exports.standalone = navigator.standalone;
+/**
+ * The device is mobile or not
+ * @type {Boolean} mobile
+ */
+export const mobile = /Android|webOS|iPhone|Windows Phone|iPod|iPad|BlackBerry|SymbianOS/i.test(userAgent);
+
+/**
+ * The browser is safari or not
+ * @type {Boolean} safari
+ */
+export const safari = /\bversion\/([0-9.]+(?: beta)?)(?: mobile(?:\/[a-z0-9]+)?)? safari\//i.test(userAgent);
+
+/**
+ * Language
+ * @type {String} language
+ */
+export const language = /^zh/i.test(navigator.language) ? 'zh' : 'en';
