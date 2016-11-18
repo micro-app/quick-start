077a278580a9ac6a3adfd66a6a7dd756f9146f07
李昕亮 <lixinliang@yy.com>
2016-11-19 00:30:00 +0800

    update
    vue action

--- src/entry/modules/vue-action.js
+++ src/entry/modules/vue-action.js
@@ -1,17 +1,23 @@
-module.exports = function ( Vue ) {
+const className = 'is-active';
+
+export default function ( Vue ) {
     Vue.directive('action', {
-        bind : function () {
+        bind () {
+            function activation () {
+                this.classList.add(className);
+            };
+            function deactivation () {
+                this.classList.remove(className);
+            };
+            this.el.addEventListener('touchstart', activation, false);
+            this.el.addEventListener('touchend', deactivation, false);
+            this.el.addEventListener('touchcancel', deactivation, false);
+            this.unbind = this.unbind.bind(this, { activation, deactivation });
         },
-        update : function () {
-            if (this.arg == 'active') {
-                let className = 'is-active';
-                this.el.addEventListener('touchstart', function () {
-                    this.classList.add(className);
-                }, false);
-                this.el.addEventListener('touchend', function () {
-                    this.classList.remove(className);
-                }, false);
-            }
+        unbind ({ activation, deactivation }) {
+            this.el.removeEventListener('touchstart', activation, false);
+            this.el.removeEventListener('touchend', deactivation, false);
+            this.el.removeEventListener('touchcancel', deactivation, false);
         },
     });
 };
