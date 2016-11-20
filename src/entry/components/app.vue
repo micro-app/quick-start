d9b1f5735d33750c2f2d839ffddc9a16b1d98880
李昕亮 <lixinliang@yy.com>
2016-11-20 18:29:46 +0800

    update
    app

--- src/entry/components/app.vue
+++ src/entry/components/app.vue
@@ -1,82 +1,46 @@
 <script>
-import Vue from 'vue';
 import {
 	mobile,
-	standalone,
+	language,
 } from '../modules/user-agent.js';
-import { lang } from '../modules/lang.js';
-import tips from './tips.vue';
-import finish from './finish.vue';
 import topbar from './topbar.vue';
-import install from './install.vue';
-import profile from './profile.vue';
-import welcome from './welcome.vue';
+import clipper from './clipper.vue';
 
-const app_className = {
-	en : 'en',
-	zh : 'zh',
-};
-const app_landscapeTips = {
+const lang = language;
+
+const landscapeTips = ({
 	en : 'Please rotate your device to portrait mode.',
 	zh : '请在竖屏下操作',
-};
-
-Vue.transition('fade', {
-	enter () {
-		this.$broadcast('enter:' + this.currentPage);
-	},
-});
-
-let history = [];
+})[lang];
 
 export default {
 	components : {
-		tips,
-		finish,
 		topbar,
-		install,
-		profile,
-		welcome,
+		clipper,
 	},
 	data () {
-		let img = new Image;
 		return {
-			standalone,
-			currentPage : 'welcome',
-			landscapeTips : app_landscapeTips[lang],
-		}
+			clipping : false,
+		};
 	},
-	methods : {
-		show ( page ) {
-			history.push(this.currentPage);
-			this.currentPage = page;
-		},
-		back () {
-			this.currentPage = history.pop();
-		},
-	},
-	computed : {
-		className () {
-			return `lang-${ app_className[lang] } ${ mobile ? 'device-mobile' : 'device-pc' }`;
-		},
+	ready () {
+		this.$el.classList.add(`lang-${ lang }`);
+		this.$el.classList.add(`device-${ mobile ? 'mobile' : 'pc' }`);
+		this.$el.setAttribute('tips', landscapeTips);
 	},
 };
 </script>
 
 <template>
-	<div id="app" v-if="!standalone" :class="className" :tips="landscapeTips">
-		<component
-			keep-alive
-			:is="currentPage"
-			transition="fade"
-		></component>
+	<div id="app">
+		<router-view transition="fade"></router-view>
+		<clipper :clipping.sync="clipping"></clipper>
 		<topbar></topbar>
 	</div>
 </template>
 
 <style lang="scss">
-	$baseColor : #3995EE;
-	$activeColor : #2485E3;
+	@import "../sass/_variable.scss";
 	html,
 	body,
 	#app {
@@ -89,7 +53,7 @@ export default {
 		*,
         *::before,
         *::after {
-            font-weight: 400;
+            // font-weight: 400;
 			-webkit-touch-callout: none;
 		}
 		*:not(input) {
@@ -99,28 +63,23 @@ export default {
 	            user-select: none;
 			}
 		}
-		.app-page {
-			top: 0;
-	        left: 0;
-	        width: 100%;
-	        height: 100%;
-	        overflow: hidden;
-	        position: absolute;
-			background: #fdfdfd;
-			&.fade-transition {
-				transition: opacity 400ms linear;
-			}
-			&.fade-enter,
-			&.fade-leave {
-				opacity: 0;
-			}
-		}
 		&.device-pc {
 			min-height: 600px;
 			@media only screen and (max-width: 414px) {
 	            min-height: 480px;
 	        }
 		}
+	}
+	.app-page {
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		overflow: hidden;
+		position: absolute;
+		background: #fdfdfd;
+	}
+	#app {
 		&.device-mobile {
 			@media only screen and (max-height: 414px) and (orientation : landscape) {
 				.app-page {
@@ -139,30 +98,32 @@ export default {
 				}
 	        }
 		}
-	    .app-button {
-	        position: absolute;
-	        left: 50%;
-	        transform: translateX(-50%);
-	        background: $baseColor;
-	        cursor: pointer;
-	        color: #fff;
-	        bottom: 80px;
-	        padding: 0 12px + 3;
-	        height: 42px + 12;
-	        line-height: 42px + 12;
-	        font-size: 20px + 8;
-	        border-radius: 5px + 2;
-			@media only screen and (max-width: 414px) {
-				& {
-					bottom: 30px + 3;
-					padding: 0 12px;
-					height: 42px;
-					line-height: 42px;
-					font-size: 20px;
-					border-radius: 5px;
-				}
+	}
+	.app-button {
+		position: absolute;
+		left: 50%;
+		transform: translateX(-50%);
+		background: $baseColor;
+		cursor: pointer;
+		color: #fff;
+		bottom: 80px;
+		padding: 0 12px + 3;
+		height: 42px + 12;
+		line-height: 42px + 12;
+		font-size: 20px + 8;
+		border-radius: 5px + 2;
+		@media only screen and (max-width: 414px) {
+			& {
+				bottom: 30px - 2;
+				padding: 0 12px;
+				height: 42px;
+				line-height: 42px;
+				font-size: 20px;
+				border-radius: 5px;
 			}
-	    }
+		}
+	}
+	#app {
 		&.device-mobile {
 			.app-button.is-active {
 				background: $activeColor;
@@ -172,6 +133,13 @@ export default {
 			.app-button:hover {
 				background: $activeColor;
 			}
+		}
+	}
+	.fade-transition {
+		transition: opacity 250ms linear;
+		&.fade-enter,
+		&.fade-leave {
+			opacity: 0;
 		}
 	}
 </style>
